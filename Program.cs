fea559e988f26371928ccbd09b6b378344630883
Emre Can Sari <cansarrr2@gmail.com>
2022-04-08 18:07:51 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("ilk sayıyı girin.");
+             int a = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("ikinci sayıyı girin.");
+             int b = Convert.ToInt32(Console.ReadLine());
+
+                 if(a>b)
+                 {
+                     Console.WriteLine(a +">" +b);
+                 }
+                 else if(a<b)
+                 {
+                     Console.WriteLine(a +"<"+ b);
+                
+                 }
+                 else
+                 {
+                     Console.WriteLine(a +"="+ b);
+                 }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
